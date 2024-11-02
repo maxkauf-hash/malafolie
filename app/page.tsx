9b0f111225809ae86a7e82574881b9ff98cf29a7
Maxime Kaufmann <maximekaufmann@MacBook-Air-de-Maxime.local>
2024-11-02 15:01:27 +0100

    nextjs ecommerce boilerplate

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="flex justify-center items-center min-h-screen">TITRE DE LA PAGE D'ACCUEIL DE LA BOUTIQUE MALAFOLIE</h1>
+  );
+}
